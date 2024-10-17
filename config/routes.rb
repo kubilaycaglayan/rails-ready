4a555b4c908469b3417a9e4bbe5c9118ac8f7fa3
kubilaycaglayan <kblycaglayan@gmail.com>
2024-10-17 13:36:57 +0300

    Require auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "users#index"
   devise_for :users
-  resources :users
+  resources :users, except: %i[new create edit update destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
