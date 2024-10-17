a15d5506d61ccf9a8c3d81849faac862c8e2de0c
kubilaycaglayan <kblycaglayan@gmail.com>
2024-10-17 18:13:44 +0300

    Create channel files

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,20 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    private
+    def find_verified_user
+      if verified_user = User.find_by(id: cookies.encrypted[:user_id])
+        puts "verified user " + verified_user
+        verified_user
+      else
+        puts "unverified user " + cookies.encrypted[:user_id]
+        reject_unauthorized_connection
+      end
+    end
   end
 end
